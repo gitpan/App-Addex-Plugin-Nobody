2b39909f7989b64c3e9a31be517af9a1bca71e4a
Ricardo SIGNES <rjbs@cpan.org>
2013-06-19 23:43:33 +0000

    Import of RJBS/App-Addex-Plugin-Nobody-0.004 from CPAN.
    gitpan-cpan-distribution: App-Addex-Plugin-Nobody
    gitpan-cpan-version:      0.004
    gitpan-cpan-path:         RJBS/App-Addex-Plugin-Nobody-0.004.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/App/Addex/Plugin/Nobody.pm
+++ lib/App/Addex/Plugin/Nobody.pm
@@ -2,48 +2,13 @@ use strict;
 use warnings;
 
 package App::Addex::Plugin::Nobody;
+{
+  $App::Addex::Plugin::Nobody::VERSION = '0.004';
+}
 use 5.006; # our
 use Sub::Install;
+# ABSTRACT: automatically add a recipient that goes nowhere
 
-=head1 NAME
-
-App::Addex::Plugin::Nobody - automatically add a recipient that goes nowhere
-
-=head2 VERSION
-
-version 0.003
-
-=cut
-
-our $VERSION = '0.003';
-
-=head1 DESCRIPTION
-
-The only valid "To" header that doesn't imply delivery somewhere
-looks something like this:
-
-  To: undisclosed-recipients: ;
-
-This plugin adds a virtual entry to your address book with that address.
-
-=head1 CONFIGURATION
-
-First, you have to add the plugin to your Addex configuration file's top
-section:
-
-  plugin = App::Addex::Plugin::Nobody
-
-You can supply the following options for the plugin:
-
-  name  - the "full name" to use (default: "Undisclosed Recipients")
-  nick  - the nick (if any) to provide (default: nobody)
-  group - the name of the address group (default: undisclosed-recipients)
-          this option is not well-validated, so maybe you should leave it alone
-
-The entry will have a true C<skip_hiveminder> field, to avoid bizarre
-interactions with the Hiveminder plugin.
-
-=cut
 
 sub import {
   my ($mixin, %arg) = @_;
@@ -84,23 +49,55 @@ sub import {
   });
 }
 
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+App::Addex::Plugin::Nobody - automatically add a recipient that goes nowhere
+
+=head1 VERSION
+
+version 0.004
+
+=head1 DESCRIPTION
+
+The only valid "To" header that doesn't imply delivery somewhere
+looks something like this:
+
+  To: undisclosed-recipients: ;
+
+This plugin adds a virtual entry to your address book with that address.
+
+=head1 CONFIGURATION
+
+First, you have to add the plugin to your Addex configuration file's top
+section:
+
+  plugin = App::Addex::Plugin::Nobody
+
+You can supply the following options for the plugin:
+
+  name  - the "full name" to use (default: "Undisclosed Recipients")
+  nick  - the nick (if any) to provide (default: nobody)
+  group - the name of the address group (default: undisclosed-recipients)
+          this option is not well-validated, so maybe you should leave it alone
+
+The entry will have a true C<skip_hiveminder> field, to avoid bizarre
+interactions with the Hiveminder plugin.
+
 =head1 AUTHOR
 
-Ricardo SIGNES, C<< <rjbs@cpan.org> >>
+Ricardo SIGNES <rjbs@cpan.org>
 
-=head1 BUGS
+=head1 COPYRIGHT AND LICENSE
 
-Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org>.  I will be notified, and then you'll automatically be
-notified of progress on your bug as I make changes.
+This software is copyright (c) 2008 by Ricardo SIGNES.
 
-=head1 COPYRIGHT
-
-Copyright 2008 Ricardo Signes, all rights reserved.
-
-This program is free software; you may redistribute it and/or modify it
-under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
-
-1;
